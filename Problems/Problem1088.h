e85720934b947c53355bc815f2b3d3332933c065
Vergilken <fengk@fortinet.com>
2019-09-10 13:13:12 -0700

    confusing number checked finished

--- Problems/Problem1088.h
+++ Problems/Problem1088.h
@@ -6,6 +6,20 @@
 #define LEETCODE_PROBLEM1088_H
 
 class ConfusingNumber2 {
-
+private:
+    static bool confusing_number(int num) {
+        vector<char>rot(256, 'x');
+        // init function
+        rot['0'] = '0', rot['1'] = '1', rot['6'] = '9', rot['8'] = '8', rot['9'] = '6';
+        string str = to_string(num), reversedStr;
+        for (auto c: str) {
+            if (rot[c] == 'x') {
+                return false;
+            }
+            reversedStr += rot[c];
+        }
+        reverse(begin(reversedStr), end(reversedStr));
+        return stoi(reversedStr) == num;
+    }
 };
 #endif //LEETCODE_PROBLEM1088_H
