bacbbcb00965b2c006e475877f1765022252bc64
Vergilken <fengk@fortinet.com>
2019-09-12 09:32:05 -0700

    Problem 1088 finishied

--- Problems/Problem1088.h
+++ Problems/Problem1088.h
@@ -6,8 +6,15 @@
 #define LEETCODE_PROBLEM1088_H
 
 class ConfusingNumber2 {
+public:
+     int confusing_number_num(int n) {
+        for (auto x: {1, 6 ,8, 9}) {
+            helper(x, n);
+        }
+        return count;
+    }
 private:
-    static bool confusing_number(int num) {
+    static bool check_confusing_number(int num) {
         vector<char>rot(256, 'x');
         // init function
         rot['0'] = '0', rot['1'] = '1', rot['6'] = '9', rot['8'] = '8', rot['9'] = '6';
@@ -21,5 +28,16 @@ private:
         reverse(begin(reversedStr), end(reversedStr));
         return stoi(reversedStr) == num;
     }
+
+    void helper(long long x, int n) {
+        if (x > n) return;
+        if (check_confusing_number(x)) ++count;
+        for (auto y: elements) {
+            helper(x * 10 + y, n);
+        }
+    }
+
+    const vector<int> elements = {0, 1 ,6 ,8 ,9};
+    int count = 0;
 };
 #endif //LEETCODE_PROBLEM1088_H
