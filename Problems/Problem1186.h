5a7e4e7df617b31d4ef0157803b12c5f25c46736
Vergilken <fengk@fortinet.com>
2019-09-13 14:46:40 -0700

    Problem 1186 has been finished

--- Problems/Problem1186.h
+++ Problems/Problem1186.h
@@ -7,7 +7,31 @@
 class MaxSubarraySumWithOneDeletion {
 public:
     int maximumSum(vector<int>& arr) {
+        int arrSize = arr.size();
+        vector<int> maxEndHere(arrSize, 0);
+        vector<int> maxStartHere(arrSize, 0);
+        int result = arr[0];
+        maxEndHere[0] = arr[0];
+        for (size_t i = 1; i < arrSize; ++i) {
+            maxEndHere[i] = max(maxEndHere[i-1] + arr[i], arr[i]);
+            result = max(result, maxEndHere[i]);
+        }
 
+        maxStartHere[arrSize-1] = arr[arrSize-1];
+
+        // the value of size_t cannot be self-decreased to -1
+        for (int i = arrSize - 2; i >= 0; --i) {
+            maxStartHere[i] = max(maxStartHere[i+1] + arr[i], arr[i]);
+        }
+
+        for (size_t i = 1; i < arrSize - 1; ++i) {
+            result = max(result, maxEndHere[i-1] + maxStartHere[i+1]);
+        }
+
+        return result;
     }
+
+private:
+
 };
 #endif //ALGORITHMS_PROBLEM1186_H
