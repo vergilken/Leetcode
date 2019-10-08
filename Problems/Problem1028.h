21b72dcf7f4d94341237b9ccb20f4113dc08b26b
Vergilken <fengk@fortinet.com>
2019-10-08 13:54:50 -0700

    Problem 1028 added

--- Problems/Problem1028.h
+++ Problems/Problem1028.h
@@ -5,17 +5,42 @@
 #ifndef LEETCODE_PROBLEM1028_H
 #define LEETCODE_PROBLEM1028_H
 
+#include "../Utils/stl_tools.h"
 struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    explicit TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
 };
 
 class RecoverTreeFromPreorderTraversal {
 public:
     TreeNode* recover_from_preorder(string S) {
+        vector<TreeNode*> st;
+        for (size_t i = 0, level, val; i < S.size();) {
+            for (level = 0; S[i] == '-'; ++i) {
+                ++level;
+            }
 
+            for (val = 0; i < S.size() && S[i] != '-'; ++i) {
+                val = val * 10 + S[i] - '0';
+            }
+
+            TreeNode* node = new TreeNode(val);
+            while(st.size() > level) {
+                st.pop_back();
+            }
+            if (!st.empty()) {
+                if (!(st.back() -> left)) {
+                    st.back() -> left = node;
+                } else {
+                    st.back() -> right = node;
+                }
+            }
+
+            st.push_back(node);
+        }
+        return st[0];
     }
 };
 
