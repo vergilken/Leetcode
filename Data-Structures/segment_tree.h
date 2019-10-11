1418f3a26f3175e51281ace1671be1d749ccdac1
ApolloBoxMacbookPro2 <>
2019-10-11 15:31:20 -0700

    reset segment tree

--- Data-Structures/segment_tree.h
+++ Data-Structures/segment_tree.h
@@ -6,30 +6,5 @@
 #define LEETCODE_SEGMENT_TREE_H
 
 #include "../Utils/stl_tools.h"
-struct SegmentTreeNode {
-    int start_;
-    int end_;
-    int sum_;
-    SegmentTreeNode* left_;
-    SegmentTreeNode* right_;
-    explicit SegmentTreeNode(int _start, int _end, int _sum, SegmentTreeNode* _left, SegmentTreeNode* _right) {
-        start_ = _start, end_ = _end, sum_ = _sum;
-        left_ = _left, right_ = _right;
-    }
-};
 
-class SegmentTree {
-public:
-    explicit SegmentTree() = default;
-    SegmentTreeNode* build_tree(int start, int end, vector<int>& values) {
-        if (start == end) {
-            return new SegmentTreeNode(start, end, values[start], nullptr, nullptr);
-        }
-
-        int mid = (start + end) / 2;
-        auto left = build_tree(start, mid, values);
-        auto right = build_tree(mid + 1, end, values);
-        return new SegmentTreeNode(start, end, left -> sum_ + right -> sum_, left, right);
-    }
-};
 #endif //LEETCODE_SEGMENT_TREE_H
