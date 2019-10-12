1d5e193970284170a87e6dbe14dd534a42478702
Vergilken <fengk@fortinet.com>
2019-10-11 17:47:52 -0700

    printer on binary tree updated

--- Data-Structures/segment_tree.h
+++ Data-Structures/segment_tree.h
@@ -7,4 +7,21 @@
 
 #include "../Utils/stl_tools.h"
 
+class SegmentTreeNode {
+public:
+    SegmentTreeNode(int start, int end, int sum,
+            SegmentTreeNode* left = nullptr,
+            SegmentTreeNode* right = nullptr):
+            start(start), end(end), sum(sum), left(left), right(right) {}
+    SegmentTreeNode(const SegmentTreeNode&) = delete;
+    SegmentTreeNode& operator=(const SegmentTreeNode&) = delete;
+
+
+public:
+    int start;
+    int end;
+    int sum;
+    SegmentTreeNode* left;
+    SegmentTreeNode* right;
+};
 #endif //LEETCODE_SEGMENT_TREE_H
