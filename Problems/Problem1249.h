eda1ee72948da6dacb01455edbe57e84c5bfa2b4
Vergilken <fengk@fortinet.com>
2019-11-04 15:51:50 -0800

    Problem 1249 added

--- Problems/Problem1249.h
+++ Problems/Problem1249.h
@@ -5,4 +5,29 @@
 #ifndef LEETCODE_PROBLEM1249_H
 #define LEETCODE_PROBLEM1249_H
 
+class MinimumRemoveToMakeValidParentheses {
+public:
+    static string min_remove_to_make_valid(string& s) {
+        stack<char> st;
+        for (size_t i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')') {
+                if (!st.empty()) {
+                    st.pop();
+                } else {
+                    s[i] = '*';
+                }
+            }
+
+            while (!st.empty()) {
+                s[st.top()] = '*';
+                st.pop();
+            }
+
+            s.erase(remove(s.begin(), s.end(), "*"), s.end());
+            return s;
+        }
+    }
+};
 #endif //LEETCODE_PROBLEM1249_H
