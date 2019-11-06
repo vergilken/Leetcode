2097c9d42b1be473e4c0a64fe86d07c2ee1116c9
Vergilken <fengk@fortinet.com>
2019-11-05 16:23:57 -0800

    Problem 1248 added

--- Problems/Problem1248.h
+++ Problems/Problem1248.h
@@ -8,7 +8,23 @@
 class CountNumberOfNiceSubarrays {
 public:
     static int number_of_subarrays(vector<int>& nums, int k) {
+        at_most_K(nums, k) - at_most_K(nums, k - 1);
+    }
 
+private:
+    static int at_most_K(vector<int>& nums, int k) {
+        int res = 0;
+        size_t i = 0;
+
+        for (size_t j = 0; j < nums.size(); ++j) {
+            k -= nums[j] % 2;
+            while (k < 0) {
+                k += nums[i++] % 2;
+            }
+            res += j - i + 1;
+        }
+
+        return res;
     }
 };
 #endif //LEETCODE_PROBLEM1248_H
