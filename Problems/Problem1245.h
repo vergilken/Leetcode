c09658b9e094726ef037de6137e7c05c95369455
Vergilken <fengk@fortinet.com>
2019-11-06 17:20:38 -0800

    Problem 1245 updated

--- Problems/Problem1245.h
+++ Problems/Problem1245.h
@@ -7,8 +7,39 @@
 
 class TreeDiameter {
 public:
-    static int tree_diameter(vector<vector<int>>& edges) {
+    int tree_diameter(vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> mp;
+        for (auto & edge : edges) {
+            mp[edge[0]].push_back(edge[1]);
+            mp[edge[1]].push_back(edge[0]);
+        }
 
+        int init = edges[0][0];
+        reset_distance();
+        int start = helper(mp, init, 0);
+        reset_distance(); visited.clear();
+        helper(mp, start, 0);
+        return distance;
     }
+
+private:
+    int helper(unordered_map<int, vector<int>>& mp, int node, int tempDist) {
+        vector<int> neighbors = mp[node];
+        int result = node;
+        for (int neighbor : neighbors) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                result = helper(mp, neighbor, tempDist + 1);
+            }
+        }
+        distance = max(distance, tempDist);
+        return result;
+    }
+
+    void reset_distance() {
+        distance = 0;
+    }
+    int distance;
+    unordered_map<int, bool> visited;
 };
 #endif //ALGORITHMS_PROBLEM1245_H
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "solution.h"
 
 int main() {
-
+    vector<vector<int>> edges = {{1, 2}, {2, 3}, {1, 4}};
+    auto temp = new TreeDiameter();
+    cout << "final result: " <<  temp -> tree_diameter(edges) << endl;
     return EXIT_SUCCESS;
 }
