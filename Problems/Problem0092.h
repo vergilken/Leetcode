73c8be71aee387808d0f0fef92a0bf6e8124d373
冯垦 <ken@theapollobox.com>
2020-09-06 07:14:14 -0700

    problem 0092 added

---
+++ Problems/Problem0092.h
@@ -0,0 +1,33 @@
+//
+// Created by Vergilken on 9/4/20.
+//
+
+#ifndef LEETCODE_PROBLEM0092_H
+#define LEETCODE_PROBLEM0092_H
+class ReverseLinkedList2 {
+public:
+    ForwardLinkedListNode<int>* reverse_linked_list_between(ForwardLinkedListNode<int>* head, int left, int right) {
+        if (left == right) {
+            return head;
+        }
+
+        auto dummy = new ForwardLinkedListNode<int>(-1);
+        dummy -> next = head;
+
+        auto prev = dummy, cur = head;
+        for (int i = 0; i < m - 1; ++i) {
+            prev = prev -> next;
+            cur = cur -> next;
+        }
+
+        for (int i = 0; i < n - m; ++i) {
+            auto next = cur -> next;
+            cur -> next = next -> next;
+            next -> next = prev -> next;
+            prev -> next = next;
+        }
+
+        return dummy -> next;
+    }
+};
+#endif //LEETCODE_PROBLEM0092_H
