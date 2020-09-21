313f9ac7bfbaff362ed634c197041a6d4588ca8c
vergilken <fengk@fortinet.com>
2020-09-21 00:10:31 -0700

    Problem 1367 finished

--- Problems/Problem1367.h
+++ Problems/Problem1367.h
@@ -5,4 +5,27 @@
 #ifndef LEETCODE_PROBLEM1367_H
 #define LEETCODE_PROBLEM1367_H
 
+class LinkedListInBinaryTree {
+public:
+    static bool is_sub_path(ForwardLinkedListNode<int>* head, Node<int>* root) {
+        if (!root) return false;
+        if (check_path(head, root)) return true;
+        bool left = is_sub_path(head, root -> left);
+        bool right = is_sub_path(head, root -> right);
+        return left or right;
+    }
+private:
+    static bool check_path(ForwardLinkedListNode<int>* head, Node<int>* root) {
+        if (!head) {
+            return true;
+        } else if (!root && head) {
+            return false;
+        } else {
+            bool flag = head -> value == root -> value;
+            bool left = check_path(head -> next, root -> left);
+            bool right = check_path(head -> next, root -> right);
+            return flag && (left || right);
+        }
+    }
+};
 #endif //LEETCODE_PROBLEM1367_H
