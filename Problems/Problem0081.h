0b77e1e29b3d5d40fc5c04b0d6f0e8bf09738d7a
vergilken <fengk@fortinet.com>
2020-10-12 23:32:53 -0700

    problem 81 finished

--- Problems/Problem0081.h
+++ Problems/Problem0081.h
@@ -8,7 +8,37 @@
 class SearchInRotatedArray2 {
 public:
     static int search(const vector<int>& nums, int target) {
+        if (nums.size() == 0 || nums.size() == 1 && nums[0] != target) {
+            return false;
+        }
 
+        size_t left = 0, right = nums.size() - 1;
+        while (left + 1 != right) {
+            auto mid = (left + right) >> 1;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] > nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            } else if (nums[left] > nums[mid]){
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            } else {
+                ++left;
+            }
+        }
+
+        if (nums[left] == target || nums[right] == target) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 #endif //LEETCODE_PROBLEM0081_H
